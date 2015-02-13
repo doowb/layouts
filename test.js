0b4d6aeaa1461b12effcb9e72a9508ed4173d0c4
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 15:47:25 -0500

    move tests

--- test.js
+++ test.js
@@ -8,7 +8,7 @@
 'use strict';
 
 var should = require('should');
-var layouts = require('..');
+var layouts = require('./');
 
 describe('.layouts():', function () {
 
@@ -94,18 +94,18 @@ describe('.layouts():', function () {
       ddd: {content: 'ddd above\n{% foo %}\nddd below', locals: {title: 'Baz'} }
     };
     var stack3 = {
-      'default': {content: 'default above\n[[ body ]]\ndefault below', locals: {title: 'Quux'}},
-      aaa: {content: 'aaa above\n[[ body ]]\naaa below', locals: {title: 'Foo'}, layout: 'bbb'},
-      bbb: {content: 'bbb above\n[[ body ]]\nbbb below', locals: {title: 'Bar'}, layout: 'ccc'},
-      ccc: {content: 'ccc above\n[[ body ]]\nccc below', locals: {title: 'Baz'}, layout: 'default'},
-      ddd: {content: 'ddd above\n[[ body ]]\nddd below', locals: {title: 'Baz'} }
+      'default': {content: 'default above\n{{ body }}\ndefault below', locals: {title: 'Quux'}},
+      aaa: {content: 'aaa above\n{{ body }}\naaa below', locals: {title: 'Foo'}, layout: 'bbb'},
+      bbb: {content: 'bbb above\n{{ body }}\nbbb below', locals: {title: 'Bar'}, layout: 'ccc'},
+      ccc: {content: 'ccc above\n{{ body }}\nccc below', locals: {title: 'Baz'}, layout: 'default'},
+      ddd: {content: 'ddd above\n{{ body }}\nddd below', locals: {title: 'Baz'} }
     };
     var stack4 = {
-      'default': {content: 'default above\n[[ foo ]]\ndefault below', locals: {title: 'Quux'}},
-      aaa: {content: 'aaa above\n[[ foo ]]\naaa below', locals: {title: 'Foo'}, layout: 'bbb'},
-      bbb: {content: 'bbb above\n[[ foo ]]\nbbb below', locals: {title: 'Bar'}, layout: 'ccc'},
-      ccc: {content: 'ccc above\n[[ foo ]]\nccc below', locals: {title: 'Baz'}, layout: 'default'},
-      ddd: {content: 'ddd above\n[[ foo ]]\nddd below', locals: {title: 'Baz'} }
+      'default': {content: 'default above\n{{ foo }}\ndefault below', locals: {title: 'Quux'}},
+      aaa: {content: 'aaa above\n{{ foo }}\naaa below', locals: {title: 'Foo'}, layout: 'bbb'},
+      bbb: {content: 'bbb above\n{{ foo }}\nbbb below', locals: {title: 'Bar'}, layout: 'ccc'},
+      ccc: {content: 'ccc above\n{{ foo }}\nccc below', locals: {title: 'Baz'}, layout: 'default'},
+      ddd: {content: 'ddd above\n{{ foo }}\nddd below', locals: {title: 'Baz'} }
     };
 
     it('should use a custom tag', function () {
@@ -123,12 +123,12 @@ describe('.layouts():', function () {
     });
 
     it('should use custom delimiters', function () {
-      layouts(stack3.aaa.content, 'bbb', stack3, {delims: ['[[', ']]']}).should.eql([
+      layouts(stack3.aaa.content, 'bbb', stack3, {layoutDelims: ['{{', '}}']}).should.eql([
         'default above',
         'ccc above',
         'bbb above',
         'aaa above',
-        '[[ body ]]',
+        '{{ body }}',
         'aaa below',
         'bbb below',
         'ccc below',
@@ -137,17 +137,90 @@ describe('.layouts():', function () {
     });
 
     it('should use custom delimiters and tag', function () {
-      layouts(stack4.aaa.content, 'bbb', stack4, {tag: 'foo', delims: ['[[', ']]']}).should.eql([
+      layouts(stack4.aaa.content, 'bbb', stack4, {tag: 'foo', layoutDelims: ['{{', '}}']}).should.eql([
         'default above',
         'ccc above',
         'bbb above',
         'aaa above',
-        '[[ foo ]]',
+        '{{ foo }}',
         'aaa below',
         'bbb below',
         'ccc below',
         'default below'
       ].join('\n'));
     });
+  });
+});
+
+
+describe('layout delimiters', function () {
+  var stack = {
+    'default': {
+      content: 'default above\n{% body %}\ndefault below',
+      locals: {title: 'Quux'}
+    },
+    aaa: {
+      content: 'aaa above\n{% body %}\naaa below',
+      locals: {title: 'Foo'},
+      layout: 'bbb'
+    },
+    bbb: {
+      content: 'bbb above\n{% body %}\nbbb below',
+      locals: {title: 'Bar'},
+      layout: 'ccc'
+    },
+    ccc: {
+      content: 'ccc above\n{% body %}\nccc below',
+      locals: {title: 'Baz'},
+      layout: 'default'
+    },
+    ddd: {
+      content: 'ddd above\n{% body %}\nddd below',
+      locals: {title: 'Baz'}
+    }
+  };
+
+  it('should apply a layout to the given string.', function () {
+    var obj = {blah: {content: 'blah above\n{% body %}\nblah below'}};
+    layouts('This is content', 'blah', obj).should.eql([
+      'blah above',
+      'This is content',
+      'blah below'
+    ].join('\n'));
+  });
+  var stack = {
+    'default': {
+      content: 'default above\n{% body %}\ndefault below',
+      locals: {title: 'Quux'}
+    },
+    aaa: {
+      content: 'aaa above\n{% body %}\naaa below',
+      locals: {title: 'Foo'},
+      layout: 'bbb'
+    },
+    bbb: {
+      content: 'bbb above\n{% body %}\nbbb below',
+      locals: {title: 'Bar'},
+      layout: 'ccc'
+    },
+    ccc: {
+      content: 'ccc above\n{% body %}\nccc below',
+      locals: {title: 'Baz'},
+      layout: 'default'
+    },
+    ddd: {
+      content: 'ddd above\n{% body %}\nddd below',
+      locals: {title: 'Baz'}
+    }
+  };
+
+  it('should use default delimiters', function () {
+    var obj = {abc: {content: '{%= body %}[[body]]{%body%}{% body %}<%body%>'}};
+    layouts('INNER', 'abc', obj).should.eql('{%= body %}[[body]]{%body%}INNER<%body%>');
+  });
+
+  it('should use custom delimiters', function () {
+    var obj = {abc: {content: '{%= body %}[[body]]{%body%}{% body %}<%body%>'}};
+    layouts('INNER', 'abc', obj, {layoutDelims: ['<%', '%>']}).should.eql('{%= body %}[[body]]{%body%}{% body %}INNER');
   });
 });

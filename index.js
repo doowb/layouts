6a11ac87e07a052be21742fbe8d30d7851798e46
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-17 01:31:48 -0400

    allow buffers to be passed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var isFalsey = require('falsey');
+var Buffer = require('buffer').Buffer;
 var delims = require('delimiter-regex');
 var get = require('get-value');
 
@@ -8,65 +9,94 @@ var get = require('get-value');
  * Expose `layouts`
  */
 
-module.exports = layouts;
+module.exports = renderLayouts;
 
 /**
- * Wrap a string one or more layouts.
+ * Cache compiled delimiter regex.
  *
- * @param {String} `str` The content string to be wrapped with a layout.
- * @param {String} `key` The object key of the starting layout.
- * @param {Object} `templates` Object of layouts.
- * @param {Object} `options`
- *     @option {Object} [options] `layoutDelims` Custom delimiters to use.
- *     @option {Object} [options] `defaultLayout` The name (key) of the default layout to use.
- * @return {String} String wrapped with a layout or layouts.
+ * If delimiters need to be generated, this ensures that
+ * runtime compilation only happens once.
+ */
+
+var cache = {};
+
+/**
+ * Wrap one or more layouts around `string`.
+ *
+ * ```js
+ * renderLayouts(string, layoutName, layoutStack, options, fn);
+ * ```
+ *
+ * @param  {String} `string` The string to wrap with a layout.
+ * @param  {String} `layoutName` The name (key) of the layout object to use.
+ * @param  {Object} `layoutStack` Object of layout objects.
+ * @param  {Object} `options` Optionally define a `defaultLayout` (string), pass custom delimiters (`layoutDelims`) to use as the placeholder for the content insertion point, or change the name of the placeholder tag with the `tag` option.
+ * @param  {Function} `fn` Optionally pass a function to modify the context as each layout is applied.
+ * @return {String} Returns the original string wrapped with one or more layouts.
  * @api public
  */
 
-function layouts(str, key, templates, opts, fn) {
+function renderLayouts(str, name, layoutStack, opts, fn) {
+  if (isBuffer(str)) {
+    str = str.toString();
+  }
+
   if (typeof str !== 'string') {
-    throw new TypeError('layouts expects a string');
+    throw new TypeError('layouts expects a string.');
   }
 
   if (typeof opts === 'function') {
-    fn = opts; opts = {};
+    fn = opts;
+    opts = {};
   }
 
   opts = opts || {};
-  var template = {}, prev, i = 0;
-  var res = {options: {}, stack: []};
+  var layout = {};
+  var depth = 0;
+  var prev;
 
-  while (key && (prev !== key) && (template = templates[key])) {
+  // `view` is the object we'll use to store the result
+  var view = {options: {}, history: []};
+
+  // recursively resolve layouts
+  while (name && (prev !== name) && (layout = layoutStack[name])) {
     var delims = opts.layoutDelims;
 
-    // `context` is passed to `interpolate` to resolve templates
-    // to the values on the context object.
-    var context = {};
-    context[opts.tag || 'body'] = str;
+    // `data` is passed to `wrapLayout` to resolve layouts
+    // to the values on the data object.
+    var data = {};
+    data[opts.tag || 'body'] = str;
 
-    // get the context for the layout and push it onto `stack`
+    // get info about the current layout
     var obj = {};
-    obj.layout = template;
-    obj.layout.key = key;
+    obj.layout = layout;
+    obj.layout.name = name;
     obj.before = str;
-    obj.depth = i++;
+    obj.depth = depth++;
 
-    str = interpolate(template.content, context, delims);
+    // inject the string into the layout
+    str = wrapLayout(layout.content, data, delims);
     obj.after = str;
 
+    // if a (sync) callback is passed, allow it modify
+    // the result in place
     if (typeof fn === 'function') {
-      fn(obj, res, i);
+      fn(obj, view, depth);
     }
 
-    res.stack.push(obj);
-    prev = key;
-    key = assertLayout(template.layout, opts.defaultLayout);
+    // push info about the layout onto `history`
+    view.history.push(obj);
+    prev = name;
+
+    // should we recurse again?
+    // (does the `layout` itself specify another layout?)
+    name = assertLayout(layout.layout, opts.defaultLayout);
   }
 
-  res.options = opts;
-  res.result = str;
-  return res;
-};
+  view.options = opts;
+  view.result = str;
+  return view;
+}
 
 /**
  * Assert whether or not a layout should be used based on
@@ -91,26 +121,17 @@ function assertLayout(value, defaultLayout) {
 }
 
 /**
- * Cache compiled regexps to prevent runtime
- * compilation for the same delimiter strings
- * multiple times (this trick can be used for
- * any compiled regex)
- */
-
-var cache = {};
-
-/**
  * Resolve template strings to the values on the given
- * `context` object.
+ * `data` object.
  */
 
-function interpolate(content, context, syntax) {
+function wrapLayout(content, data, syntax) {
   var re = makeDelimiterRegex(syntax);
-  return toString(content).replace(re, function(_, $1) {
-    if ($1.indexOf('.') !== -1) {
-      return toString(get(context, $1.trim()));
+  return toString(content).replace(re, function(_, tagName) {
+    if (tagName.indexOf('.') !== -1) {
+      return toString(get(data, tagName.trim()));
     }
-    return context[$1.trim()];
+    return data[tagName.trim()];
   });
 }
 
@@ -126,12 +147,12 @@ function makeDelimiterRegex(syntax) {
   if (syntax instanceof RegExp) {
     return syntax;
   }
+  var name = syntax + '';
+  if (cache.hasOwnProperty(name)) {
+    return cache[name];
+  }
   if (typeof syntax === 'string') {
     return new RegExp(syntax, 'g');
-  }
-  var key = syntax.toString();
-  if (cache.hasOwnProperty(key)) {
-    return cache[key];
   }
   if (Array.isArray(syntax)) {
     return (cache[syntax] = delims(syntax));
@@ -142,6 +163,11 @@ function makeDelimiterRegex(syntax) {
  * Cast `val` to a string.
  */
 
-function toString(val){
+function toString(val) {
   return val == null ? '' : val.toString();
+}
+
+function isBuffer(val) {
+  return typeof val === 'object'
+    && val instanceof Buffer;
 }

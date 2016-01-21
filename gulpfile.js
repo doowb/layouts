1f429ef6fdabfb53b0a6b21c246514b17d080a6e
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-21 06:05:04 -0500

    use eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,29 @@
+'use strict';
+
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
-var jshint = require('gulp-jshint');
-
-var lint = ['index.js', 'utils.js'];
+var eslint = require('gulp-eslint');
 
 gulp.task('coverage', function() {
-  return gulp.src(lint)
-    .pipe(istanbul())
+  return gulp.src(['index.js', 'utils.js', 'lib/*.js'])
+    .pipe(istanbul({includeUntested: true}))
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function() {
-  return gulp.src('test.js')
-    .pipe(mocha({reporter: 'spec'}))
-    .pipe(istanbul.writeReports())
+gulp.task('mocha', ['coverage'], function() {
+  return gulp.src(['test.js', 'test/*.js'])
+    .pipe(mocha())
     .pipe(istanbul.writeReports({
-      reporters: [ 'text' ],
+      reporters: ['html', 'text', 'text-summary'],
       reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    }));
 });
 
-gulp.task('lint', function() {
-  return gulp.src(lint)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+gulp.task('eslint', function() {
+  return gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('default', ['mocha', 'eslint']);

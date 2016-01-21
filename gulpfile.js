b4a84c118f7dd8f52c89e62014869b38aaced729
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-21 06:09:02 -0500

    clean up reporters

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,7 @@ gulp.task('coverage', function() {
 gulp.task('mocha', ['coverage'], function() {
   return gulp.src(['test.js', 'test/*.js'])
     .pipe(mocha())
-    .pipe(istanbul.writeReports({
-      reporters: ['html', 'text', 'text-summary'],
-      reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }));
+    .pipe(istanbul.writeReports());
 });
 
 gulp.task('eslint', function() {
